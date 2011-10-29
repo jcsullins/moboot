e92c2c7a5fa3b0fb76a359f39f73ee107334c1f2
James Sullins <jcsullins@gmail.com>
2011-10-29 07:34:32 -0500

    add HANDLE_LINUX_KERNEL_ARGS, set DEBUG=0

--- project/apq-touchpad.mk
+++ project/apq-touchpad.mk
@@ -5,8 +5,17 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 TARGET := apq-touchpad
 
 MODULES += app/moboot
+MODULES += lib/bootlinux
+MODULES += lib/uimage
+MODULES += lib/atags
+MODULES += lib/zlib
+MODULES += lib/fs
+MODULES += lib/fs/ext2
+MODULES += lib/gfx
+MODULES += lib/gfxconsole
+MODULES += lib/tga
 
-DEBUG := 2
+DEBUG := 0
 
 #DEFINES += WITH_DEBUG_DCC=1
 #DEFINES += WITH_DEBUG_UART=1
@@ -15,3 +24,4 @@ DEFINES += WITH_DEV_FBCON=1
 DEFINES += DISPLAY_TYPE_TOUCHPAD=1
 DEFINES += _EMMC_BOOT=1
 DEFINES += DISABLE_MMC_DEBUG_SPEW
+DEFINES += HANDLE_LINUX_KERNEL_ARGS
